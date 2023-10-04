e2cf429309f6c240d933270a8cf9836c49d3ff1c
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2023-10-05 01:15:42 +0300

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const gallery = document.querySelector('.js-gallery');
+const markupGallery = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+        />
+    </a>
+    </li>
+`
+  )
+  .join('');
+
+  gallery.innerHTML = markupGallery;
+
+  let lightbox  = new SimpleLightbox('.gallery a');
+  
+  lightbox.options.captionsData = 'title';
+  lightbox.options.captionDelay = 250;
+  console.log(lightbox);
